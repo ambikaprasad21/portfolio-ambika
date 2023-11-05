54a7fced072475fc9761d7e3233977fcf0443177
Ambika Prasad <akashboard2021@gmail.com>
2023-11-06 00:44:35 +0530

    form updated

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,10 @@ function Header() {
   return (
     <header className="header">
       <div className="section-header">
-        <p className="header-right header-text">Ambika Prasad</p>
+        {/* <p className="header-right header-text">Ambika Prasad</p>
         <div className="header-left">
           <p className="about header-text">About</p>
-        </div>
+        </div> */}
       </div>
       <Hero />
     </header>
