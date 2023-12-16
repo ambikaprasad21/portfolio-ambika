5bbc684ddca92833a3e43e5557be5e3f922e4c95
Ambika Prasad <akashboard2021@gmail.com>
2023-12-17 02:33:12 +0530

    name bold

--- src/About.js
+++ src/About.js
@@ -32,13 +32,13 @@ function About() {
         <div className="about-content">
           <div className="about-text about-item">
             <p className="intro-para">
-              My name is Ambika Prasad, a passionate web developer with a drive
-              for turning ideas into digital experiences. With a deep-rooted
-              love for all things web, I thrive on crafting elegant and
-              functional websites that not only look stunning but also deliver
-              seamless user experiences. Let's collaborate to bring your web
-              projects to life and make the internet a more beautiful and
-              user-friendly place!
+              My name is <span style={{ fontWeight: 600 }}>Ambika Prasad</span>,
+              a passionate web developer with a drive for turning ideas into
+              digital experiences. With a deep-rooted love for all things web, I
+              thrive on crafting elegant and functional websites that not only
+              look stunning but also deliver seamless user experiences. Let's
+              collaborate to bring your web projects to life and make the
+              internet a more beautiful and user-friendly place!
             </p>
             <div className="box-info">
               <div className="box-left">
